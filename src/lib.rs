98079c51918235c3a240b59ef28db6e29c67c419
Avery R <contact@nekodjin.xyz>
2021-09-07 12:39:53 -0400

    add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 pub mod unrestricted;
 
+/// Duplicate Mutable Reference
+///
+/// This function returns a mutable reference to the value behind another
+/// reference, mutable or immutable.
 pub unsafe fn dup_mut<'a, T>(brw: &T) -> &'a mut T {
     let tmp: *const T = brw;
     &mut *(tmp as *mut T)
 }
 
+/// Duplicate Immutable Reference
+///
+/// This function returns an immutable reference to the value behind another
+/// reference, mutable or immutable.
 pub unsafe fn dup_imm<'a, T>(brw: &T) -> &'a T {
     let tmp: *const T = brw;
     &*tmp
--- src/unrestricted.rs
+++ src/unrestricted.rs
@@ -1,8 +1,22 @@
+//! Unrestricted Reference Duplications
+//!
+//! This submodule contains the same functions as its parent module, but
+//! they are not marked as unsafe. Using this submodule in production codebases
+//! is strongly discouraged.
+
+/// Duplicate Mutable Reference
+///
+/// This function creates a mutable reference to the value behind another
+/// reference, mutable or immutable.
 pub fn dup_mut<'a, T>(brw: &T) -> &'a mut T {
     let tmp: *const T = brw;
     unsafe { &mut *(tmp as *mut T) }
 }
 
+/// Duplicate Immutable Reference
+///
+/// This function creates an immutable reference to the value behind another
+/// reference, mutable or immutable.
 pub fn dup_imm<'a, T>(brw: &T) -> &'a T {
     let tmp: *const T = brw;
     unsafe { &*tmp }
